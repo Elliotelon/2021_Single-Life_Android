3bbc4d93c7dfb49ae20c6c6d8be1ac4037b2e514
kimminkyu <kmk50009@naver.com>
2021-07-16 00:22:46 +0900

    SingleLife 메인화면 TopBar UI

--- app/src/main/java/study/singlelife/MainActivity.kt
+++ app/src/main/java/study/singlelife/MainActivity.kt
@@ -20,12 +20,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.logoutBtn).setOnClickListener {
-            auth.signOut()
-
-            val intent = Intent(this, IntroActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            startActivity(intent)
-        }
+//        findViewById<Button>(R.id.logoutBtn).setOnClickListener {
+//            auth.signOut()
+//
+//            val intent = Intent(this, IntroActivity::class.java)
+//            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+//            startActivity(intent)
+//        }
     }
 }
--- app/src/main/java/study/singlelife/SplashActivity.kt
+++ app/src/main/java/study/singlelife/SplashActivity.kt
@@ -4,17 +4,39 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import study.singlelife.auth.IntroActivity
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        auth = Firebase.auth
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        //3초후 IntroActivity로 액티비티가 이동됨.
-        Handler().postDelayed({
-            startActivity(Intent(this, IntroActivity::class.java))
-            finish()
-        }, 3000)
+        if(auth.currentUser?.uid == null){
+            Log.d("SplashActivity", "null")
+            //3초후 IntroActivity로 액티비티가 이동됨.
+            Handler().postDelayed({
+                startActivity(Intent(this, IntroActivity::class.java))
+                finish()
+            }, 3000)
+        }else {
+            Log.d("SplashActivity", "not null")
+            //3초후 MainActivity로 액티비티가 이동됨.
+            Handler().postDelayed({
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }, 3000)
+
+        }
+
     }
 }
