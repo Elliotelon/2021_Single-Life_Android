5725c07d812c5d50819a2e763b1b74d152fac560
kimminkyu <kmk50009@naver.com>
2021-07-15 21:56:01 +0900

    SingleLife 회원가입, 로그인 페이지 UI

--- app/src/main/java/study/singlelife/auth/IntroActivity.kt
+++ app/src/main/java/study/singlelife/auth/IntroActivity.kt
@@ -1,12 +1,28 @@
 package study.singlelife.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import study.singlelife.R
+import study.singlelife.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityIntroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_intro)
+
+        binding.loginBtn.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.joinBtn.setOnClickListener {
+            val intent = Intent(this , JoinActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/study/singlelife/auth/LoginActivity.kt
@@ -0,0 +1,12 @@
+package study.singlelife.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import study.singlelife.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
